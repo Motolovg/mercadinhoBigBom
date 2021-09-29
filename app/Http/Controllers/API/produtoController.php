8d55c5ec160c4bc412ed8635952e5e7affa90e6e
motolovg <giovanediasmiranda@gmail.com>
2021-09-29 18:30:29 -0300

    produto controller

---
+++ app/Http/Controllers/API/produtoController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Produto;
+use Illuminate\Http\Request;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProdutoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(produto::all());
+    }
+
+    public function store(Request $request)
+    {
+      produto::create($request->all());
+    }
+
+    
+    public function show($id)
+    {
+        return produto::findOrfail($id);
+    }
+
+    public function update(Request $request, produto $produto)
+    {
+        $produto = produto::findOrfail($id);
+        $produto->update($request->all());
+    }
+
+    public function destroy(produto $produto)
+    {
+        $produto = produto::findOrfail($id);
+        $produto->delete();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('produtos', 'App\Http\Controllers\API\produtoController@index');
+Route::post('produtos', 'App\Http\Controllers\API\produtoController@store');
