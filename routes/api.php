63963369b6994aed7356b73101aa0e8f86945e91
motolovg <giovanediasmiranda@gmail.com>
2021-09-30 18:20:17 -0300

    tentativa api get com id

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('produtos', 'App\Http\Controllers\API\produtoController@index');
-Route::post('produtos', 'App\Http\Controllers\API\produtoController@store');
-Route::put('produtos', 'App\Http\Controllers\API\produtoController@update');
-Route::delete('produtos', 'App\Http\Controllers\API\produtoController@destroy');
+Route::get('/produtos', 'App\Http\Controllers\API\produtoController@index');
+Route::get('/produtos {id}', 'App\Http\Controllers\API\produtoController@show');
+Route::post('/produtos', 'App\Http\Controllers\API\produtoController@store');
+Route::put('/produtos', 'App\Http\Controllers\API\produtoController@update');
+Route::delete('/produtos', 'App\Http\Controllers\API\produtoController@destroy');
